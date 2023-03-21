9d4d5760d14ddd40b0f25005509308d50768b58e
MartiVallhonrat <martivallhonrat@gmail.com>
2023-03-21 13:01:39 +0100

    display pressupostList w buttons

--- S7.-Angular-ll/src/app/home/home.component.ts
+++ S7.-Angular-ll/src/app/home/home.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { InteractionService } from '../interaction.service';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormControlDirective, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import {formatDate} from '@angular/common';
 
 interface Budget {
-  budgetName: any;
-  clientName: any;
-  isCheckedWeb: any;
-  isCheckedSEO: any;
-  isCheckedAds: any;
+  budgetName: string | null | undefined;
+  clientName: string | null | undefined;
+  isCheckedWeb: boolean | null | undefined;
+  isCheckedSEO: boolean | null | undefined;
+  isCheckedAds: boolean | null | undefined;
   totalPrice: number;
+  date: string;
 }
 
 @Component({
@@ -19,10 +21,12 @@ interface Budget {
 
 export class HomeComponent implements OnInit {
 
-  constructor(private _panelMessageSource: InteractionService) {}
+  constructor(
+    private _panelMessageSource: InteractionService
+  ) {}
 
 
-  public plusPriceWeb:number = 30;
+  public plusPriceWeb: number = 30;
   public totalPrice = 0;
   public pressupostList: Budget[] = [];
   public submited: boolean = false;
@@ -41,22 +45,10 @@ export class HomeComponent implements OnInit {
       .subscribe(
         resultMessage => {
           this.plusPriceWeb = resultMessage;
-          console.log(this.plusPriceWeb)
           this.checkTotal()
         }
       )
   }
-
-  public setClear() {
-    this.budgetForm.value.budgetName = "";
-    this.budgetForm.value.clientName = "";
-    this.budgetForm.value.isCheckedWeb = false;
-    this.budgetForm.value.isCheckedSEO = false;
-    this.budgetForm.value.isCheckedAds = false;
-    this.plusPriceWeb = 30;
-    this.checkTotal();
-  }
-  
 
   public checkTotal() {
 
@@ -65,7 +57,6 @@ export class HomeComponent implements OnInit {
 
     if(this.budgetForm.value.isCheckedWeb) {
       price = price + (500 + this.plusPriceWeb);
-      console.log(this.plusPriceWeb)
     } else {
       this.plusPriceWeb = 30;
     }
@@ -81,9 +72,7 @@ export class HomeComponent implements OnInit {
 
   public pushPressupostList() {
 
-    this.submited = true;
-
-    if(!this.budgetForm.controls['clientName'].errors?.['required'] && !this.budgetForm.controls['budgetName'].errors?.['required']) {
+    if(this.budgetForm.valid) {
 
       this.pressupostList.push({
         budgetName: this.budgetForm.value.budgetName,
@@ -91,15 +80,18 @@ export class HomeComponent implements OnInit {
         isCheckedWeb: this.budgetForm.value.isCheckedWeb,
         isCheckedSEO: this.budgetForm.value.isCheckedSEO,
         isCheckedAds: this.budgetForm.value.isCheckedAds,
-        totalPrice: this.totalPrice
+        totalPrice: this.totalPrice,
+        date: formatDate(new Date(), 'yyyy/MM/dd', 'en')
       })
 
-      this.setClear();
+      this._panelMessageSource.renderPressupostList(this.pressupostList);
+
+      this.budgetForm.reset();
+      this.submited = false;
+      this.checkTotal();
+    } else {
+
+      this.submited = true;
     }
-
-    console.log(this.pressupostList)
-    this._panelMessageSource.renderPressupostList(this.pressupostList);
-    
-
   }
 }
--- S7.-Angular-ll/src/app/home/pressupost-list/pressupost-list.component.ts
+++ S7.-Angular-ll/src/app/home/pressupost-list/pressupost-list.component.ts
@@ -8,14 +8,17 @@ import { InteractionService } from 'src/app/interaction.service';
 })
 export class PressupostListComponent implements OnInit {
 
+  public pressupostListDefault: any;
   public pressupostList: any;
+  public enteredSearchValue: string = "";
 
   ngOnInit() {
     this._panelMessageSource.panelMessage$
       .subscribe(
         resultArray => {
-          this.pressupostList = resultArray;
-          console.log(this.pressupostList)
+          this.pressupostListDefault = resultArray;
+          console.log(this.pressupostListDefault)
+          this.printPressuposts("buttonDefault");
         }
       )
   }
@@ -23,4 +26,40 @@ export class PressupostListComponent implements OnInit {
   constructor(
     private _panelMessageSource: InteractionService
   ) {}
+
+  public orderAlfabethicly() {
+
+    let alfaArray = [...this.pressupostListDefault];
+    alfaArray.sort((a, b) => a.budgetName.localeCompare(b.budgetName));
+    return alfaArray;
+  }
+
+  public orderDate() {
+    
+    let dateArray = [...this.pressupostListDefault];
+    dateArray.sort((a, b) => a.date - b.date);
+    return dateArray;
+  }
+
+  public printPressuposts(title: string) {
+
+    if(title == "buttonAlfa") {
+
+      this.pressupostList = this.orderAlfabethicly();
+    }
+    if(title == "buttonDate") {
+      
+      this.pressupostList = this.orderDate();
+    }
+    if(title == "buttonDefault") {
+      
+      this.pressupostList = this.pressupostListDefault;
+    }
+
+  }
+  
+  public onSearchValue(search: string) {
+
+    this.enteredSearchValue = search;
+  }
 }
